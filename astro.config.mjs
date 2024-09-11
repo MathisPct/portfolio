caf6862f353c9beab174e436a8c8933b4e7052a1
MathisPct <math712b@gmail.com>
2024-09-12 01:20:04 +0200

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mathispct.github.io',
-	base: '/portfolio',
+	site: 'https://portfolio.mathispct.fr',
 	integrations: [mdx(), sitemap()],
 });
